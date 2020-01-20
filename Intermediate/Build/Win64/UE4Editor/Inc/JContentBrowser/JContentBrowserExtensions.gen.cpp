6199db611f5657c4aa7c274a8d46ef719e8ff49f
Ramon Janousch <ramon.j@gmx.de>
2020-01-20 11:38:37 +0100

    [Added] Binaries for 4.24

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/JContentBrowser/JContentBrowserExtensions.gen.cpp
@@ -0,0 +1,86 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "JContentBrowser/Public/JContentBrowserExtensions.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeJContentBrowserExtensions() {}
+// Cross Module References
+	JCONTENTBROWSER_API UClass* Z_Construct_UClass_UJUnusedAssetsSearchFilter_NoRegister();
+	JCONTENTBROWSER_API UClass* Z_Construct_UClass_UJUnusedAssetsSearchFilter();
+	CONTENTBROWSER_API UClass* Z_Construct_UClass_UContentBrowserFrontEndFilterExtension();
+	UPackage* Z_Construct_UPackage__Script_JContentBrowser();
+// End Cross Module References
+	void UJUnusedAssetsSearchFilter::StaticRegisterNativesUJUnusedAssetsSearchFilter()
+	{
+	}
+	UClass* Z_Construct_UClass_UJUnusedAssetsSearchFilter_NoRegister()
+	{
+		return UJUnusedAssetsSearchFilter::StaticClass();
+	}
+	struct Z_Construct_UClass_UJUnusedAssetsSearchFilter_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_UJUnusedAssetsSearchFilter_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_UContentBrowserFrontEndFilterExtension,
+		(UObject* (*)())Z_Construct_UPackage__Script_JContentBrowser,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UJUnusedAssetsSearchFilter_Statics::Class_MetaDataParams[] = {
+		{ "Comment", "/**\n * Register custom  filters with the content browser\n */" },
+		{ "IncludePath", "JContentBrowserExtensions.h" },
+		{ "ModuleRelativePath", "Public/JContentBrowserExtensions.h" },
+		{ "ToolTip", "Register custom  filters with the content browser" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_UJUnusedAssetsSearchFilter_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<UJUnusedAssetsSearchFilter>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_UJUnusedAssetsSearchFilter_Statics::ClassParams = {
+		&UJUnusedAssetsSearchFilter::StaticClass,
+		nullptr,
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x000000A0u,
+		METADATA_PARAMS(Z_Construct_UClass_UJUnusedAssetsSearchFilter_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_UJUnusedAssetsSearchFilter_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_UJUnusedAssetsSearchFilter()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_UJUnusedAssetsSearchFilter_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(UJUnusedAssetsSearchFilter, 1294784620);
+	template<> JCONTENTBROWSER_API UClass* StaticClass<UJUnusedAssetsSearchFilter>()
+	{
+		return UJUnusedAssetsSearchFilter::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_UJUnusedAssetsSearchFilter(Z_Construct_UClass_UJUnusedAssetsSearchFilter, &UJUnusedAssetsSearchFilter::StaticClass, TEXT("/Script/JContentBrowser"), TEXT("UJUnusedAssetsSearchFilter"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(UJUnusedAssetsSearchFilter);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/JContentBrowser/JContentBrowserExtensions.generated.h
@@ -0,0 +1,95 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef JCONTENTBROWSER_JContentBrowserExtensions_generated_h
+#error "JContentBrowserExtensions.generated.h already included, missing '#pragma once' in JContentBrowserExtensions.h"
+#endif
+#define JCONTENTBROWSER_JContentBrowserExtensions_generated_h
+
+#define CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_SPARSE_DATA
+#define CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_RPC_WRAPPERS
+#define CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_RPC_WRAPPERS_NO_PURE_DECLS
+#define CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUJUnusedAssetsSearchFilter(); \
+	friend struct Z_Construct_UClass_UJUnusedAssetsSearchFilter_Statics; \
+public: \
+	DECLARE_CLASS(UJUnusedAssetsSearchFilter, UContentBrowserFrontEndFilterExtension, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/JContentBrowser"), NO_API) \
+	DECLARE_SERIALIZER(UJUnusedAssetsSearchFilter)
+
+
+#define CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_INCLASS \
+private: \
+	static void StaticRegisterNativesUJUnusedAssetsSearchFilter(); \
+	friend struct Z_Construct_UClass_UJUnusedAssetsSearchFilter_Statics; \
+public: \
+	DECLARE_CLASS(UJUnusedAssetsSearchFilter, UContentBrowserFrontEndFilterExtension, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/JContentBrowser"), NO_API) \
+	DECLARE_SERIALIZER(UJUnusedAssetsSearchFilter)
+
+
+#define CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UJUnusedAssetsSearchFilter(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UJUnusedAssetsSearchFilter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UJUnusedAssetsSearchFilter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UJUnusedAssetsSearchFilter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UJUnusedAssetsSearchFilter(UJUnusedAssetsSearchFilter&&); \
+	NO_API UJUnusedAssetsSearchFilter(const UJUnusedAssetsSearchFilter&); \
+public:
+
+
+#define CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UJUnusedAssetsSearchFilter(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UJUnusedAssetsSearchFilter(UJUnusedAssetsSearchFilter&&); \
+	NO_API UJUnusedAssetsSearchFilter(const UJUnusedAssetsSearchFilter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UJUnusedAssetsSearchFilter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UJUnusedAssetsSearchFilter); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UJUnusedAssetsSearchFilter)
+
+
+#define CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_PRIVATE_PROPERTY_OFFSET
+#define CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_13_PROLOG
+#define CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_PRIVATE_PROPERTY_OFFSET \
+	CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_SPARSE_DATA \
+	CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_RPC_WRAPPERS \
+	CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_INCLASS \
+	CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_PRIVATE_PROPERTY_OFFSET \
+	CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_SPARSE_DATA \
+	CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_RPC_WRAPPERS_NO_PURE_DECLS \
+	CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_INCLASS_NO_PURE_DECLS \
+	CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h_17_ENHANCED_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> JCONTENTBROWSER_API UClass* StaticClass<class UJUnusedAssetsSearchFilter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID CommerzReal_Plugins_JContentBrowser_Source_JContentBrowser_Public_JContentBrowserExtensions_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
